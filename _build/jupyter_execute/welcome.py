a0c976f3e8da1d3782ee85b57bf2610d410ef237
Robert Kinsella <chefk67@gmail.com>
2020-11-30 10:31:11 -0500

    version 1

--- _build/jupyter_execute/welcome.py
+++ _build/jupyter_execute/welcome.py
@@ -3,17 +3,18 @@
 
 # # The VRC Phoenix Skills Only Challenge
 # 
+# ## VRC Change Up
 # 
-# ## VRC Change Up!
 # December 5th 2020
 # 
 # ```{image} ./images/PSARobotics.jpg
 # :alt: psarobologo
 # :align: center
 # ```
+# 
 # Welcome to The VRC Phoenix Winter Qualifier! We look forward to a competitive and exciting VRC Change Up tournament which will include virtual judged awards.  Only competitors and registered coaches will be permitted in the team area in order to maintain safe social distancing.  Spectators will not be permitted to attend.  The event will be live streamed on YouTube.
 # 
-# ### All PSA Robotics events can be viewed live at [Twitch](https://twitch.tv/vexteam_9447) on the day of the event and replays can be found on [YouTube](https://www.youtube.com/channel/UCD4qMvWYb7rBnB3L6p7BZAQ/).
+# ### All PSA Robotics events can be viewed live at [Twitch](https://twitch.tv/vexteam_9447) on the day of the event and replays can be found on [YouTube](https://www.youtube.com/channel/UCD4qMvWYb7rBnB3L6p7BZAQ/)
 # 
 # ![qrcode](./images/twitch_page.png)
 # 
